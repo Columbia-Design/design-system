019a2b8abaa1fc4358324243a0410ea9b9371adf
Aaron Pinero <aaronpinero@gmail.com>
2020-01-20 17:54:51 -0500

    DS-11: working on hero variations

--- src/react-components/hero.js
+++ src/react-components/hero.js
@@ -6,14 +6,16 @@ class Hero extends React.Component {
     super(props);
   }
   render() {
-    const classes = `hero ${this.props.type}`;
+    const classes = `hero ${this.props.class}`;
     return (
 			<div className={classes}>
-				<img class="d-block w-100" border="0" alt={this.props.alt} src={this.props.image} />
-        <div class="hero-body">
-				  <h1 class="hero-header">{renderHTML(this.props.heading)}</h1>
-				  <div class="hero-text">{renderHTML(this.props.text)}</div>
-				  <a class="btn btn-primary d-inline-block" href="#">{this.props.cta}</a>
+        <div class="hero-img">
+				  <img border="0" alt={this.props.alt} src={this.props.image} />
+        </div>
+        <div class="hero-body p-4">
+				  <h1>{renderHTML(this.props.heading)}</h1>
+				  <p class="lead">{renderHTML(this.props.text)}</p>
+				  <a class="btn btn-primary btn-lg d-inline-block" href="#">{this.props.cta}</a>
         </div>
 			</div>
     );
