24cf61166b8c8d960e480f0d35202c278ae4e5e1
lev soroka <ls3031@columbia.edu>
2020-07-30 11:01:03 -0400

    Updates

---
+++ precache-manifest.180fa3d310e672b6694e98bf733e9ebb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18acae10ae05d049be5a2a6e5dcc7878",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38396462f0c0dbb82afe",
+    "url": "/static/css/2.6ea54503.chunk.css"
+  },
+  {
+    "revision": "eb67dbc1522bb1db302d",
+    "url": "/static/css/main.45b697c7.chunk.css"
+  },
+  {
+    "revision": "38396462f0c0dbb82afe",
+    "url": "/static/js/2.e97caeec.chunk.js"
+  },
+  {
+    "revision": "3e77249d76369ca8341792ec0c147d3c",
+    "url": "/static/js/2.e97caeec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb67dbc1522bb1db302d",
+    "url": "/static/js/main.b9feb262.chunk.js"
+  },
+  {
+    "revision": "8909ce6e706c776f402d",
+    "url": "/static/js/runtime-main.3ef291d2.js"
+  },
+  {
+    "revision": "88755e686979948448af96d5520b7c30",
+    "url": "/static/media/cu-logo.88755e68.svg"
+  },
+  {
+    "revision": "38a7dfc10e9db443b162ca8664599c95",
+    "url": "/static/media/cuicons.38a7dfc1.woff"
+  },
+  {
+    "revision": "396a7e0348bc7bcbb2ba65269d4864ab",
+    "url": "/static/media/cuicons.396a7e03.ttf"
+  },
+  {
+    "revision": "4444377abbac27371be36b559622ed38",
+    "url": "/static/media/cuicons.4444377a.woff2"
+  },
+  {
+    "revision": "b678f09163cf262c509585b2a5824bec",
+    "url": "/static/media/cuicons.b678f091.svg"
+  },
+  {
+    "revision": "b7f5a11b90186290419ae455ab10c684",
+    "url": "/static/media/cuicons.b7f5a11b.eot"
+  },
+  {
+    "revision": "73be22254c8f47c89bde0474fd31568f",
+    "url": "/static/media/logo-black.73be2225.jpg"
+  }
+]);
